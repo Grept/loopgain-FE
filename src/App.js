600a8e3ee1f7ccb9665c2b7bd792762fa6eb7a71
Grept <tomjansen.408@gmail.com>
2022-04-14 13:13:47 +0200

    Made initial projectfiles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
 import './App.css';
+import {Switch, Route} from "react-router-dom";
+
+import Navbar from "./components/NavBar/NavBar";
+import HomePage from "./pages/home/HomePage";
 
 function App() {
-  return (
-    <></>
-  );
+    return (
+        <>
+            <Navbar />
+            <div className="content">
+                <Switch>
+                    <Route exact path="/">
+                        <HomePage />
+                    </Route>
+
+                </Switch>
+            </div>
+        </>
+    );
 }
 
 export default App;
